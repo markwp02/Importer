00039f5328f4da65d32e256ca6c65fc2340f5b03
Mark <markwp02@gmail.com>
2024-01-24 12:10:31 +1100

    Initial commit

---
+++ src/test/java/com/markp/Importer/ImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markp.Importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
